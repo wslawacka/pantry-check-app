20650ac689dc546ac976b9c89dd9978486d168c2
wslawacka <slawacka@wp.pl>
2025-05-13 14:28:55 +0200

    Implement authentication middleware and add it to pantry item route

--- backend/routes/pantryItemRoutes.js
+++ backend/routes/pantryItemRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { addPantryItem } = require('../controllers/pantryItemController');
 const { pantryItemValidation } = require('../middleware/pantryItemValidator');
+const authenticationMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/', pantryItemValidation, addPantryItem);
+router.post('/', authenticationMiddleware, pantryItemValidation, addPantryItem);
 
 module.exports = router;
